70593c67f8d0e2e88b530dc5b4c97061f0135029
Vijay Savakhande <vijay.savakhande2000@gmail.com>
2018-06-13 12:11:57 +0530

    test changes in file

--- index.php
+++ index.php
@@ -3,13 +3,12 @@ $method = $_SERVER['REQUEST_METHOD'];
 if ($method =='POST') {
   $request_body = file_get_contents('php://input');
   $json = json_decode($request_body,True);
-  print_r($json);
-  // $text = $json->queryResult->parameters->{geo-city}
-  
-  // $response = new \stdClass();
-  // $response->speech ="";
-  // $response->displayText="";
-  // $response->source="webhook";
+  $city = $json['queryResult']['parameters']['geo-city'];
+  $speech = $city.' Are you sure is this metro city?';  
+  $response = new \stdClass();
+  $response->speech ="";
+  $response->displayText="";
+  $response->source="webhook";
   echo json_encode($response);
 }else{
   echo 'not a post method';
